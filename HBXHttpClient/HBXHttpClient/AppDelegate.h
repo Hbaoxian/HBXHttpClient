7e7af38c75d4175621d79e37776a341d53a850ea
黄保贤 <huangbaoxian@edaijia-inc.cn>
2016-05-26 18:17:21 +0800

    提交工程

---
+++ HBXHttpClient/HBXHttpClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBXHttpClient
+//
+//  Created by 黄保贤 on 16/5/26.
+//  Copyright © 2016年 黄保贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
